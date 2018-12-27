69f3741be16a6ffd44f486793e9d6bd14e034157
JRocken <jp.rockenmacher@gmail.com>
2018-12-26 19:14:02 -0700

    cleaned up app

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ new Vue({
             //this function displays e.target.value until a following keydown...
             // by default wait on key down for 1000 then will show; can circumvent it, but also can do it by keyup
             keyDownBasic: function(e) {
-                // e.force()
                 this.value = e.target.value
-                // console.log(e.target.value)
             },
             keyDownInt: function(e){
                 this.value = e.target.value
