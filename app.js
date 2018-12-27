ea61d8d7b7461204cbf6513249c27622a87c389e
JRocken <jp.rockenmacher@gmail.com>
2018-12-26 19:09:42 -0700

    changed first function to keyup for better aesthetics

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ new Vue({
         methods: {
             clickAlert: function(){
                 alert("I\'m clicked!")
+            },
+            //this function displays e.target.value until a following keydown...
+            // by default wait on key down for 1000 then will show; can circumvent it, but also can do it by keyup
+            keyDownBasic: function(e) {
+                // e.force()
+                this.value = e.target.value
+                // console.log(e.target.value)
+            },
+            keyDownInt: function(e){
+                this.value = e.target.value
             }
         }
     });
